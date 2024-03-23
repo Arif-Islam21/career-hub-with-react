292e23c3b08b47a5966c3e553a6bb5dcd8a0e2a8
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-03-23 09:57:57 +0600

    show all data btn created

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -3,6 +3,8 @@ import Job from "../Job/Job";
 
 const FeaturedJobs = () => {
   const [jobs, setJobs] = useState([]);
+  // this is not the best way to show al data
+  const [dataLength, setDataLength] = useState(4);
 
   useEffect(() => {
     fetch("jobs.json")
@@ -20,9 +22,21 @@ const FeaturedJobs = () => {
         </p>
       </div>
       <div className="grid grid-cols-2 gap-6">
-        {jobs.map((job) => (
+        {jobs.slice(0, dataLength).map((job) => (
           <Job key={job.id} job={job}></Job>
         ))}
+      </div>
+      <div
+        className={
+          dataLength === jobs.length ? "hidden" : "flex justify-center my-4"
+        }
+      >
+        <button
+          onClick={() => setDataLength(jobs.length)}
+          className="btn btn-primary"
+        >
+          Show All Jobs
+        </button>
       </div>
     </div>
   );
--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,3 +1,6 @@
+import { IoLocationSharp } from "react-icons/io5";
+import { CiDollar } from "react-icons/ci";
+
 const Job = ({ job }) => {
   const {
     logo,
@@ -24,6 +27,16 @@ const Job = ({ job }) => {
             <button className="btn px-5 py-2 font-extrabold border-2 rounded border-lime-50 mr-4 text-[#7E90FE]">
               {job_type}
             </button>
+          </div>
+          <div className="my-4 flex ">
+            <h2 className="flex text-2xl mr-16">
+              <IoLocationSharp className="" />
+              {location}
+            </h2>
+            <h2 className="text-2xl flex">
+              <CiDollar />
+              {salary}
+            </h2>
           </div>
           <div className="card-actions ">
             <button className="btn btn-primary">View Details</button>
