e6cea8c1c5060cb4085dacbb1b26a795373e5540
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-03-24 16:06:32 +0600

    working with local storage

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -1,10 +1,19 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
+import { saveJobAplication } from "../../utility/localStorage";
 
 const JobDetails = () => {
   const jobs = useLoaderData();
   const { id } = useParams();
   const idInt = parseInt(id);
   const job = jobs.find((job) => job.id === idInt);
+
+  const handleApplyJob = () => {
+    saveJobAplication(id);
+    toast("You have applied sucesfuly");
+  };
   return (
     <div>
       <div className="grid md:grid-cols-4 gap-4">
@@ -14,9 +23,12 @@ const JobDetails = () => {
         </div>
         <div className="border md:col-span-1">
           <h2 className="text-4xl">Side Panel</h2>
-          <button className="btn btn-primary w-full">Apply Now</button>
+          <button onClick={handleApplyJob} className="btn btn-primary w-full">
+            Apply Now
+          </button>
         </div>
       </div>
+      <ToastContainer />
     </div>
   );
 };
